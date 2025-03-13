de1f7d939272c181d6481650ad479af467a0e04a
Loweso <dlouisefermin@gmail.com>
2025-03-13 18:15:05 +0800

    deleted misc

--- OSIModel/_server.py
+++
@@ -1,5 +0,0 @@
-from PhysicalLayer import PhysicalLayer
-
-if __name__ == "__main__":
-    server_physical = PhysicalLayer(role="server")  # Server mode
-    server_physical.receive()  # Keep server running
